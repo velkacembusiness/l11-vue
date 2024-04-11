085198ca7b109078d94fdf0ef2ef4d547004b7eb
VelKacem LAOUI <velkacembusiness@gmail.com>
2024-04-11 23:49:11 +0100

    laravel-vue-pagination

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted } from "vue";
+import { TailwindPagination } from 'laravel-vue-pagination';
 import usePosts from "@/composables/posts";
 
 const { posts, getPosts } = usePosts()
@@ -30,7 +31,7 @@ const { posts, getPosts } = usePosts()
                 </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                    <tr v-for="post in posts">
+                    <tr v-for="post in posts.data">
                         <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
                             {{ post.id }}
                         </td>
@@ -46,6 +47,7 @@ const { posts, getPosts } = usePosts()
                     </tr>
                 </tbody>
             </table>
+            <TailwindPagination :data="posts" @pagination-change-page="getPosts" class="mt-4" />
         </div>
     </div>
 </template>
--- resources/js/composables/posts.js
+++ resources/js/composables/posts.js
@@ -1,10 +1,10 @@
 import { ref } from 'vue'
 
 export default function usePosts() {
-    const posts = ref([])
+    const posts = ref({})
 
-    const getPosts = async () => {
-        axios.get('/api/posts')
+    const getPosts = async (page = 1) => {
+        axios.get('/api/posts?page=' + page)
             .then(response => {
                 posts.value = response.data;
             })
